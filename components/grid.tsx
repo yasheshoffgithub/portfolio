6fccfe9399a480fa79d002cc1b0803c7cd320bd7
Yashesh <yashesh.pratap.ug21@nsut.ac.in>
2024-06-08 01:37:09 +0530

    BentoGrid completed

--- components/grid.tsx
+++ components/grid.tsx
@@ -1,22 +1,29 @@
 import React from 'react'
 import { BentoGrid, BentoGridItem } from './ui/BentoGrid'
+import { gridItems } from '@/data'
 
 const grid = () => {
   return (
     <section id="about">
-        <BentoGrid>
-          {[{ title: 'title1',description:'desc1',id:1}].map
-          ((item,i)=>(
-            <BentoGridItem 
-              id={item.id}
-              key={item.id}
-              title={item.title}
-              description={item.description}
-            />
-          ))}
+        <BentoGrid className="w-full py-20">
+        {gridItems.map((item, i) => (
+          <BentoGridItem
+            id={item.id}
+            key={i}
+            title={item.title}
+            description={item.description}
+            // remove icon prop
+            // remove original classname condition
+            className={item.className}
+            img={item.img}
+            imgClassName={item.imgClassName}
+            titleClassName={item.titleClassName}
+            spareImg={item.spareImg}
+          />
+        ))}
         </BentoGrid>
     </section>
-  )
-}
+  );
+};
 
-export default grid
+export default grid;

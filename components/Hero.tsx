ff49bb07f87400919f26203c6d63545cc27ed45d
Yashesh <yashesh.pratap.ug21@nsut.ac.in>
2024-06-08 23:58:32 +0530

    Project section complete

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,17 +2,23 @@ import React from 'react'
 import { Spotlight } from './ui/Spotlight';
 import { TextGenerateEffect } from './ui/text-generate-effect';
 import { MagicButton } from './ui/MagicButton';
-import { FaLocationArrow } from 'react-icons/fa';
+import { FaLocationArrow } from 'react-icons/fa6';
 
 const Hero = () => {
   return (
     <div className="pb-20 pt-36">
         <div>
-          <Spotlight className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen" fill="white" />
-          <Spotlight className="top-10 left-full h-[80vh] w-[50vw]" fill="white" />
-          <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="white" />
+        <Spotlight
+          className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
+          fill="white"
+        />
+        <Spotlight
+          className="h-[80vh] w-[50vw] top-10 left-full"
+          fill="purple"
+        />
+        <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
         </div>
-        <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
+        <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.2] absolute top-0 left-0 flex items-center justify-center">
       {/* Radial gradient for the container to give a faded look */}
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
       {/* <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
@@ -21,13 +27,15 @@ const Hero = () => {
     </div>
       <div className='flex justify-center relative my-20 z-10'>
         <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
-            <h2 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80'>Dynamic Web Magic with Next.js</h2>
+        <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+            Dynamic Web Magic with Next.js
+          </p>
             <TextGenerateEffect 
-              className='text-center text-[40px] md:text-5xl lg:text-6xl'
               words='transforming concepts into seamless experiences'
+              className='text-center text-[40px] md:text-5xl lg:text-6xl'
             />
             <p className='text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl'>
-              Hi, I'm Yashesh, a Full Stack Developer 
+              Hi, I'm Yashesh, a Full Stack Developer based in India
             </p>
             <a href="#about">
               <MagicButton 

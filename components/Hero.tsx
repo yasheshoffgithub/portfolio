878f3498a1467bafd0bd0261f794adc9f3b3272b
Yashesh <yashesh.pratap.ug21@nsut.ac.in>
2024-06-11 23:08:09 +0530

    REMOVED spotlight

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,7 +7,7 @@ import { FaLocationArrow } from 'react-icons/fa6';
 const Hero = () => {
   return (
     <div className="pb-20 pt-36">
-        <div>
+        {/* <div>
         <Spotlight
           className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
           fill="white"
@@ -18,7 +18,7 @@ const Hero = () => {
       fill="white"
     />
     
-        </div>
+        </div> */}
         <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.2] absolute top-0 left-0 flex items-center justify-center">
       {/* Radial gradient for the container to give a faded look */}
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
